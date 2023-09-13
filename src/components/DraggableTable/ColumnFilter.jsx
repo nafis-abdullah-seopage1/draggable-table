cc01a9dc257c78ab76e92dfa93f1ccb3b8cff024
nafis-abdullah-seopage1 <nafis.abdullah.seopage1>
2023-09-13 17:14:42 +0600

    column filtering problem solved

--- src/components/DraggableTable/ColumnFilter.jsx
+++ src/components/DraggableTable/ColumnFilter.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useRef, useState } from "react"
+import { columns } from "./table-utils";
 
 export default function ColumnFilter({ columns, setTableColumns }) {
     const [colsList, setColsList] = useState(columns);
+ 
+    useEffect(() => {
+        setTableColumns([...colsList]);
+    }, [colsList])
 
-    useEffect(()=>{
-        console.log(colsList);
-        setTableColumns(colsList);
-    },[colsList])
+
 
     return (
         <div className="p-3 flex justify-end">
@@ -14,41 +16,40 @@ export default function ColumnFilter({ columns, setTableColumns }) {
                 <summary className="m-1 btn btn-neutral btn-sm btn-outline">Select Filter</summary>
                 <ul className="p-2 shadow menu dropdown-content z-[1] flex-nowrap rounded bg-white border-2 w-52 space-y-1 max-h-[50vh] overflow-y-auto">
                     {
-                        [...columns].map((col, i) => {
-                            const [checked,setCheck] = useState(true);
-
-                            useEffect(()=>{
-                                if (checked) {
-                                    // console.log(colsList[i]);
-                                    if (colsList[i]?.title === col?.title) {
-                                        console.log('exist');
-                                        setCheck(prev=>prev);
-                                    } else {
-                                        console.log('not exist');
-                                        const newList = [...colsList];
-                                        setColsList(newList.splice(i,0,col));                              
-                                    }
-                                } else {
-                                    setColsList(prev=>{
-                                        const filteredList = [...prev].filter(c=>c.title!==col.title)
-                                        return filteredList;
-                                    })
-                                }
-                            },[checked])
-
-                            return (
-                                <li key={i} className="form-control">
-                                    <label htmlFor={`filter-${i}`} className="cursor-pointer label justify-start gap-2">
-                                        <input id={`filter-${i}`} name="checkbox" type="checkbox" className="checkbox" checked={checked} onChange={(e)=>setCheck(e.target.checked)}/>
-                                        <span className="text-xs">{col.content}</span>
-                                    </label>
-                                </li>
-                            )
-                        })
+                        [...columns].map((col, i) => <ColIndex key={i} col={col} i={i} setColsList={setColsList} colsList={colsList} columns={columns} />)
                     }
 
                 </ul>
             </details>
         </div>
+    )
+}
+
+
+
+const ColIndex = ({col,i,setColsList,colsList,columns}) => {
+    const [checked, setCheck] = useState(true);
+
+    useEffect(() => {
+        if (checked) {
+                const newList = [...colsList, col];
+                newList.splice(i,0,col);
+                newList.pop();
+                setColsList(newList);
+        } else {
+            setColsList(prev => {
+                const filteredList = [...prev].filter(c => c.title !== col.title)
+                return filteredList;
+            })
+        }
+    }, [checked])
+
+    return (
+        <li key={i} className="form-control">
+            <label htmlFor={`filter-${i}`} className="cursor-pointer label justify-start gap-2">
+                <input id={`filter-${i}`} name="checkbox" type="checkbox" className="checkbox" checked={checked} onChange={(e) => setCheck(e.target.checked)} />
+                <span className="text-xs">{col.content}</span>
+            </label>
+        </li>
     )
 }
