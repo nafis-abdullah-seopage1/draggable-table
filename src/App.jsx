b3232d470f9fecf751ac47f7425cb8980ca6de45
nafis-abdullah-seopage1 <nafis.abdullah.seopage1>
2023-09-13 12:21:35 +0600

    clear unused component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Header from './components/Header'
-import Table from './components/Table'
 import DraggableTable from './components/DraggableTable/DraggableTable'
 import { columns } from './components/DraggableTable/table-utils'
 
@@ -8,7 +7,6 @@ function App() {
   return (
     <main>
       <Header project_count={63} project_value={'75,600.00'}/>
-      {/* <Table columns={columns} data={data}/> */}
       <DraggableTable columns={columns} />
     </main>
   )
