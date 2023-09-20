e2a4c66a3fe5bd028a796a415b7d5dbf942b5db9
nafis-abdullah-seopage1 <nafis.abdullah.seopage1>
2023-09-20 12:09:36 +0600

    testing vibration feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import Header from './components/Header';
 import { anchored_Cell, columns } from './utils/table-utils';
 import useFetchData from './hooks/useFetchData';
 import NewDraggableTable from './components/NewDraggableTable/NewDraggableTable';
+import { Button } from 'react-bootstrap';
 
 function App() {
   const [tableData] = useFetchData('/table-data.json');
 
   return (
     <main>
+      <Button variant='dark' style={{position:'fixed',top:'0',left:'0'}} onClick={()=>{
+        window.navigator.vibrate[200];
+      }}>vibrate</Button>
       <Header project_count={63} project_value={'$75,600.00'}/>
       <NewDraggableTable columns={columns} tableData={tableData} anchored_Cell={anchored_Cell} />
     </main>
